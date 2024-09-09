a8cd588f668faaf319a68ce9de61f1bd8b9186bf
jenecilpatac <jenecilpatac@gmail.com>
2024-09-09 15:17:14 +0800

    updated User Controller

--- memo-backend/app/Http/Controllers/API/UserController.php
+++ memo-backend/app/Http/Controllers/API/UserController.php
@@ -8,6 +8,12 @@ use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ResetPasswordMail;
 
 class UserController extends Controller
 {
@@ -88,6 +94,7 @@ public function register(Request $request)
             return response()->json([
                 "status" => true,
                 "message" => "Registered Successfully",
+                'data' =>$user
             ]);
         } catch (\Throwable $th) {
             return response()->json([
@@ -214,5 +221,197 @@ public function getRole($id)
         ], 500);
     }
 }
+
+public function viewAllUsers()
+{
+    try {
+
+        $users = User::select('id', 'firstName', 'lastName')->get();
+
+        return response()->json([
+            'message' => 'Users retrieved successfully',
+            'data' => $users
+        ], 200);
+    } catch (Exception $e) {
+        return response()->json([
+            'message' => 'An error occurred while retrieving users',
+            'error' => $e->getMessage()
+        ], 500);
+    }
+}
+
+public function updateProfile(Request $request, $id)
+    {
+        try {
+            // Validate the incoming request
+            $validated = $request->validate([
+                'firstName' => 'nullable|string|max:255',
+                'lastName' => 'nullable|string|max:255',
+                'contact' => 'nullable|string|max:255',
+                'branch_code' => 'nullable',
+                'role'  => 'nullable|string|max:255',
+                'userName' => 'nullable|string|max:255',
+                'email' => 'nullable|email',
+                'position' => 'nullable|string|max:255',
+                'profile_picture' => 'nullable|file|mimes:png,jpg,jpeg',
+            ]);
+    
+            $user = User::findOrFail($id);
+    
+            DB::beginTransaction();
+    
+            // Save the profile picture if provided
+            if ($request->hasFile('profile_picture')) {
+                $profilePicture = $request->file('profile_picture');
+                $profilePicturePath = $profilePicture->store('profile_pictures', 'public');
+                $user->profile_picture = $profilePicturePath; // Save only the path
+            }
+    
+            // Update user details with provided values
+            $user->firstName = $validated['firstName'] ?? $user->firstName;
+            $user->lastName = $validated['lastName'] ?? $user->lastName;
+            $user->contact = $validated['contact'] ?? $user->contact;
+            $user->branch_code = $validated['branch_code'] ?? $user->branch_code;
+            $user->userName = $validated['userName'] ?? $user->userName;
+            $user->email = $validated['email'] ?? $user->email;
+            $user->position = $validated['position'] ?? $user->position;
+    
+            $user->save();
+    
+            DB::commit();
+    
+            return response()->json([
+                'message' => 'User updated successfully',
+                'data' => $user
+            ], 200);
+    
+        } catch (ValidationException $e) {
+            return response()->json([
+                'message' => 'Validation Error',
+                'errors' => $e->errors()
+            ], 422);
+        } catch (Exception $e) {
+            DB::rollBack();
+            return response()->json([
+                'message' => 'An error occurred while updating the user',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    
+    public function sendResetLinkEmail(Request $request)
+    {
+
+        $request->validate(['email' => 'required|email|exists:users,email']);
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user) {
+            return response()->json(['message' => 'We can\'t find a user with that email address.'], 404);
+        }
+
+        // Generate a 6-letter password
+        $newPassword = Str::random(6);
+
+        // Hash the new password
+        $hashedPassword = Hash::make($newPassword);
+
+        // Update the user's password
+        $user->password = $hashedPassword;
+        $user->save();
+
+
+        // Send the new password to the user
+        $firstname = $user->firstName;
+        Mail::to($user->email)->send(new ResetPasswordMail($newPassword, $firstname));
+
+        return response()->json([
+            'message' => 'We have sent your new password to your email.'], 200);
+
+    }
+
+
+    //RESET PASSWORD
+    public function reset(Request $request)
+    {
+
+        $request->validate([
+            'token' => 'required',
+            'email' => 'required|email|exists:users,email',
+            'password' => 'required|min:8|confirmed',
+        ]);
+
+
+        $updatePassword = DB::table('password_reset_tokens')
+            ->where([
+                "email" => $request->email,
+                "token" => $request->token,
+            ])->first();
+
+        if (!$updatePassword) {
+            return response()->json(['message' => 'Invalid.'], 400);
+        }
+
+        User::where("email", $request->email)
+            ->update(["password" => Hash::make($request->password)]);
+
+        DB::table(table: "password_reset_tokens")
+            ->where(["email" => $request->email])
+            ->delete();
+
+        return response()->json(['message' => 'Password reset successfully.'], 200);
+    }
+
+    //PROFILE - CHANGE PASSWORD
+    public function changePassword(Request $request, $id)
+    {
+        $request->validate([
+            'current_password' => 'required',
+            'new_password' => 'required|string|min:6|confirmed',
+        ]);
+
+        $user = User::findOrFail($id); // Retrieve the user by ID
+
+        // Verify if the current password matches the user's password in the database
+        if (!Hash::check($request->current_password, $user->password)) {
+            return response()->json(['error' => 'Current password is incorrect.'], 422);
+        }
+
+        // Update the user's password
+        $user->password = Hash::make($request->new_password);
+        $user->save();
+
+        return response()->json(['success' => 'Password changed successfully.'], 200);
+    }
+    //VIEW USER
+
+
+    public function viewUser($id)
+    {
+        try {
+
+            $user = User::findOrFail($id);
+
+            return response()->json([
+                'message' => 'Users retrieved successfully',
+                'data' => $user,
+                'status' => true,
+
+            ], 200);
+
+        } catch (Exception $e) {
+
+            return response()->json([
+                'message' => 'Users not found',
+            ], 404);
+
+        } catch (Exception $e) {
+
+            return response()->json([
+                'message' => 'An error occurred while retrieving the user',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
 
 }
