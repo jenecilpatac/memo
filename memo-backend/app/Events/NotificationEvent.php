542cbc9b3097a4a830fedfd0a076024fb294e610
jenecilpatac <jenecilpatac@gmail.com>
2024-09-30 13:16:15 +0800

    Notification event using Pusher

---
+++ memo-backend/app/Events/NotificationEvent.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Events\ShouldDispatchAfterCommit;
+use Illuminate\Support\Facades\Log;
+
+class NotificationEvent implements ShouldBroadcastNow
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+
+   
+
+    public $message, $user_id,$date,$type,$read_at;
+
+    public function __construct($user_id,$message,$date,$type,$read_at)
+    {
+        $this->user_id = $user_id; 
+        $this->message = $message;  
+        $this->date = $date;  
+        $this->type = $type;
+        $this->read_at = $read_at;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new Channel('notification'. $this->user_id),
+        ];
+    }
+
+    public function broadcastAs()
+    {
+        return 'notification-event';
+    }
+
+   
+}
