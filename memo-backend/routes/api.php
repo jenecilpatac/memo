043b604666f5a45eee2008505abd5d2cbfac003e
jenecilpatac <jenecilpatac@gmail.com>
2024-10-03 13:28:34 +0800

    api updated

--- memo-backend/routes/api.php
+++ memo-backend/routes/api.php
@@ -9,33 +9,33 @@ use App\Http\Controllers\API\NotificationController;
 use App\Http\Controllers\API\ExplainController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Test\PusherController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::group(['middleware' => ['role:approver']], function () {});
 
 Route::post('register',[UserController::class,"register"]);
 Route::post('login',[UserController::class,"login"]);
 Route::put('update-role',[UserController::class,'updateRole'])->name('update.user.role');
 Route::get('get-role',[UserController::class,'getRole'])->name('get.user.role');
 Route::get("view-branch", [BranchController::class,"viewBranch"])->name('view.branch');
+Route::post("password/email", [UserController::class,"sendResetLinkEmail"])->name("password.forgot");
+Route::get("view-all-users", [UserController::class,"viewAllUsers"])->name('view.all.users');
 
 Route::middleware('auth:sanctum')->group(function () {
-Route::get("view-all-users", [UserController::class,"viewAllUsers"])->name('view.all.users');
 Route::get("view-user/{id}", [UserController::class,"viewUser"])->name('view.user');
 Route::post("update-profile/{id}", [UserController::class,"updateProfile"])->name('update.profile');
-Route::post("password/email", [UserController::class,"sendResetLinkEmail"])->name("password.forgot");
 Route::post("password/reset", [UserController::class,"reset"])->name("password.reset");
 Route::put('change-password/{id}', [UserController::class, 'changePassword'])->name('change.password');
+Route::post("update-profilepic/{id}", [UserController::class,"updateProfilePic"])->name('update.profile.picture');
 
 //MEMO
 Route::group(['middleware' => ['role:Creator']], function () {
 Route::post('create-memo',[MemoController::class,'createMemo'])->name('create.memo');
 Route::post("update-memo/{id}", [MemoController::class,"updateMemo"])->name('update.memo');
 Route::get('view-memo/{currentUserId}',[MemoController::class,'viewMemo'])->name('view.memo'); });
-  
 Route::group(['middleware' => ['role:approver,User,BranchHead']], function () {
 Route::put('memo/{memo_id}/process', [ApprovalProcessController::class, 'processMemo'])->name('process.memo'); 
 Route::get('memo/for-approval/{user_id}', [ApprovalProcessController::class, 'getMemoForApproval'])->name('get.memo.for.approval');});
@@ -65,10 +65,16 @@ Route::post("add-branch", [BranchController::class,"createBranch"])->name('creat
 Route::post("update-branch/{id}", [BranchController::class,"updateBranch"])->name('update.branch');
 Route::delete("delete-branch/{id}", [BranchController::class,"deleteBranch"])->name('delete.branch');});
 
+Route::get('total-memo-sent', [UserController::class, 'totalMemoSent'])->name('total.memo.sent');
+Route::get('total-memo-received', [UserController::class, 'totalMemoReceived'])->name('total.memo.received');
+Route::get('total-explain-sent', [UserController::class, 'totalExplainSent'])->name('total.explain.sent');
+Route::get('total-explain-received', [UserController::class, 'totalExplainReceived'])->name('total.explain.received');
+
 //NOTIFICATION
 Route::get('notifications/{id}/all', [NotificationController::class, 'getAllNotifications'])->name('get.all.notification');
 Route::get('notifications/{id}', [NotificationController::class, 'getNotifications'])->name('get.notification');
 Route::get('notifications/{id}/unread', [NotificationController::class, 'getUnreadNotifications'])->name('get.unread.notification');
 Route::put('notifications/{id}/mark-as-read', [NotificationController::class, 'markAsRead'])->name('put.mark.as.read.notification');
 Route::get('notifications/{id}/count-unread-notification', [NotificationController::class, 'countUnreadNotifications'])->name('count.unread.notification');
+Route::put('notifications/mark-all-as-read/{userId}', [NotificationController::class, 'markAllAsRead'])->name('get.mark.all.as.read.notification');
 });
